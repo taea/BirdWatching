73adb2b6faecde1f3e9259cc58fad03180b28756
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-08-09 23:40:42 +0900

    Remove remaining unused methods
    あとで消すのかもしれませんが、トリのデータの登録や管理を
    BirdsMasterViewController でやらなくてよくなったので
    それ関連のメソッドを消しました。
    
    ```
    - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
    ```
    
    はまたあとで実装しそう。。

--- BirdWatching/BirdsMasterViewController.m
+++ BirdWatching/BirdsMasterViewController.m
@@ -14,13 +14,6 @@
 
 #import "BirdSighting.h"
 
-/*
-@interface BirdsMasterViewController () {
-    NSMutableArray *_objects;
-}
-@end
-*/
- 
 @implementation BirdsMasterViewController
 
 - (void)awakeFromNib
@@ -45,16 +38,6 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 #pragma mark - Table View
@@ -94,16 +77,6 @@
     return NO;
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
-
 /*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
@@ -119,14 +92,5 @@
     return YES;
 }
 */
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
-    }
-}
 
 @end
